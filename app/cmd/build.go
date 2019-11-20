14f07578abad49b019ce898cb98810c514cf6265
pgrunde <peter.a.grunde@gmail.com>
2019-11-20 13:27:25 -0700

    minimum args to 0

--- app/cmd/build.go
+++ app/cmd/build.go
@@ -11,7 +11,7 @@ var buildCmd = &cobra.Command{
 	Use:   "build",
 	Short: "Publish master for your curriculum repository",
 	Long:  `The Learn system recognizes blocks of content held in GitHub respositories. This command publishes the latest commit on master as a new release.`,
-	Args:  cobra.MinimumNArgs(1),
+	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 1 {
 			fmt.Println("Usage: `learn build` takes one argument")
