74d38eb8f08d016c6b736c55d7c2f0d1f2fd86ea
pgrunde <peter.a.grunde@gmail.com>
2019-11-20 13:57:43 -0700

    detecting branch, bailing when not on master

--- app/cmd/build.go
+++ app/cmd/build.go
@@ -2,22 +2,39 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
+var branchCommand = `git branch | grep \* | cut -d ' ' -f2`
+
 var buildCmd = &cobra.Command{
 	Use:   "build",
 	Short: "Publish master for your curriculum repository",
-	Long:  `The Learn system recognizes blocks of content held in GitHub respositories. This command publishes the latest commit on master as a new release.`,
+	Long:  `The Learn system recognizes blocks of content held in GitHub respositories. This command publishes the latest commit on master to your origin remote (which should be GitHub), then releases a new Learn block version at the HEAD of master.`,
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			fmt.Println("Usage: `learn build` takes one argument")
+		if len(args) != 0 {
+			fmt.Println("Usage: `learn build` takes no arguments, merely pushing latest master and releasing a version to Learn")
 			os.Exit(1)
 		}
 
-		fmt.Println("Called build")
+		if currentBranch() != "master" {
+			fmt.Println("You are currently not on branch 'master'- the `learn build` command must be on master branch to push all currently committed work to your 'origin master' remote.")
+		}
+		fmt.Println(currentBranch())
 	},
+}
+
+func currentBranch() string {
+	out, err := exec.Command("bash", "-c", branchCommand).Output()
+	if err != nil {
+		log.Fatal("Cannot run git branch detection with bash:", err)
+	}
+
+	return strings.TrimSpace(string(out))
 }
