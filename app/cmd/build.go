b9630c60864aaa653134bb638b5379a3ddcbc55b
pgrunde <peter.a.grunde@gmail.com>
2019-11-20 22:41:53 -0700

    refactor build, TODO notes for learn actions

--- app/cmd/build.go
+++ app/cmd/build.go
@@ -15,7 +15,7 @@ var branchCommand = `git branch | grep \* | cut -d ' ' -f2`
 var buildCmd = &cobra.Command{
 	Use:   "build",
 	Short: "Publish master for your curriculum repository",
-	Long:  `The Learn system recognizes blocks of content held in GitHub respositories. This command publishes the latest commit on master to your origin remote (which should be GitHub), then releases a new Learn block version at the HEAD of master.`,
+	Long:  `The Learn system recognizes blocks of content held in GitHub respositories. This command pushes the latest commit for the remote origin master (which should be GitHub), then attemptes the release of a new Learn block version at the HEAD of master.`,
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) != 0 {
@@ -23,37 +23,52 @@ var buildCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		branch, err := currentBranch()
-		if err != nil {
-			log.Println("Cannot run git branch detection with bash:", err)
-			os.Exit(1)
-		}
 		remote, err := remoteName()
 		if err != nil {
-			log.Println("Cannot run git branch detection with bash:", err)
+			log.Println("Cannot run git remote detection with command: git remote -v | grep push | cut -f2- -d/ | sed 's/[.].*$//'\n", err)
 			os.Exit(1)
 		}
 		if remote == "" {
 			log.Println("no fetch remote detected")
 			os.Exit(1)
 		}
-		fmt.Println("remote:", remote)
+		// fetch block by remote name, bail if not found
+		// TODO block := learn.GetBlockByRepoName(remote)
 
+		branch, err := currentBranch()
+		if err != nil {
+			log.Println("Cannot run git branch detection with bash:", err)
+			os.Exit(1)
+		}
 		if branch != "publish#169468994" { // TODO change to master before merging
 			fmt.Println("You are currently not on branch 'master'- the `learn build` command must be on master branch to push all currently committed work to your 'origin master' remote.")
 			os.Exit(1)
 		}
-		fmt.Println("Pushing work to origin remote for", branch)
+		fmt.Println("Pushing work to remote origin", branch)
 		err = pushToRemote(branch)
 		if err != nil {
 			fmt.Printf("Error pushing to origin remote on branch %s: %s", err)
 			os.Exit(1)
 		}
+		// create a release on learn, notify user
+		// TODO resp, err := learn.CreateMasterRelease(remote)
+		// if err != nil {
+		// 	fmt.Printf("", err)
+		// 	os.Exit(1)
+		// }
 	},
 }
 
 func currentBranch() (string, error) {
-	out, err := exec.Command("bash", "-c", branchCommand).Output()
+	return runBashCommand(branchCommand)
+}
+
+func remoteName() (string, error) {
+	return runBashCommand("git remote -v | grep push | cut -f2- -d/ | sed 's/[.].*$//'")
+}
+
+func runBashCommand(command string) (string, error) {
+	out, err := exec.Command("bash", "-c", command).Output()
 	if err != nil {
 		return "", err
 	}
@@ -67,15 +82,5 @@ func pushToRemote(branch string) error {
 		return err
 	}
 
-	fmt.Println(strings.TrimSpace(string(out)))
 	return nil
-}
-
-func remoteName() (string, error) {
-	out, err := exec.Command("bash", "-c", "git remote -v | grep push | cut -f2- -d/ | sed 's/[.].*$//'").Output()
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(string(out)), nil
 }
