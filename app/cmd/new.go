9301f4c5692b1b1c5dd5fe82dfd3ad573f0104dc
Chad Lillquist <chad.lillquist@galvanize.com>
2019-12-17 15:37:24 -0700

    clone into repo and move contents to current dir

--- app/cmd/new.go
+++ app/cmd/new.go
@@ -1,10 +1,14 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
-import "fmt"
-
-import "os"
+	"github.com/spf13/cobra"
+)
 
 var newCmd = &cobra.Command{
 	Use:   "new",
@@ -16,7 +20,35 @@ var newCmd = &cobra.Command{
 
 		if hasConfig {
 			fmt.Println("WARNING: configuration file detected and cannot continue with `learn new` command.")
+			os.Exit(1)
 		}
+
+		fmt.Println("Copying curriculum template from Github")
+		fmt.Println("=======================================")
+		fmt.Println("\nCloning into 'learn-curriculum-init'...")
+		err := cloneTemplate()
+		if err != nil {
+			fmt.Println("We had trouble cloning into learn-curriculum-init, please check that you have the correct github credentials")
+			os.Exit(1)
+		}
+
+		fmt.Println("Copying curriculum")
+		err = moveClonedMaterials()
+		if err != nil {
+			fmt.Println("Could not move template into working repository")
+			os.Exit(1)
+		}
+		fmt.Println("Removing cloned repo")
+
+		fmt.Println(`
+Success!
+========
+
+See repo structure guidelines in readme.md
+
+Run 'learn preview .' to preview curriculum without pushing
+Run 'learn publish' to push to Github and publish to Learn
+		`)
 	},
 }
 
@@ -46,4 +78,32 @@ func doesCurrentDirHaveConfig() bool {
 	}
 
 	return configExist
+}
+
+func cloneTemplate() error {
+	_, err := exec.Command("bash", "-c", "git clone git@github.com:gSchool/learn-curriculum-init.git").CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func moveClonedMaterials() error {
+	currentDir, _ := os.Getwd()
+	initDir := "/learn-curriculum-init"
+	os.RemoveAll(currentDir + initDir + "/.git/")
+	err := filepath.Walk(currentDir+initDir, func(path string, info os.FileInfo, err error) error {
+		if !strings.HasSuffix(path, initDir) && !strings.Contains(path, ".git/") {
+			oldLocation := path
+			newLocation := strings.Replace(path, initDir, "", 1)
+			os.Rename(oldLocation, newLocation)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	os.RemoveAll(currentDir + initDir)
+	return nil
 }
