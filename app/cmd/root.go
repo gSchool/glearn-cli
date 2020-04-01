e7d0bf1424ba5581fb604c68fee3190197687120
Chad Lillquist <chad.lillquist@galvanize.com>
2020-04-01 11:25:45 -0600

    update version

--- app/cmd/root.go
+++ app/cmd/root.go
@@ -20,7 +20,7 @@ You can get your api token at https://learn-2.galvanize.com/api_token
 `
 
 // currentReleaseVersion is used to print the version the user currently has downloaded
-const currentReleaseVersion = "v0.6.94"
+const currentReleaseVersion = "v0.7.0"
 
 // rootCmd is the base for all our commands. It currently just checks for all the
 // necessary credentials and prompts the user to set them if they are not there.
