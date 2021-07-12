4142fcf32478e3075f3c5164a4c79c2d111cce7e
Chad Lillquist <chad.lillquist@galvanize.com>
2021-07-12 11:41:53 -0600

    force push of version check

--- app/cmd/root.go
+++ app/cmd/root.go
@@ -163,5 +163,9 @@ func setupLearnAPI() {
 		return
 	}
 
+	if api.Credentials.LatestCliVersion != currentReleaseVersion {
+		fmt.Printf("\nWARNING: There is newer version of the learn tool available.\nAvailable: %s\nCurrent: %s\nTo avoid issues, upgrade by following the instructions at this link:\nhttps://github.com/gSchool/glearn-cli/blob/master/upgrade_instructions.md\n\n", api.Credentials.LatestCliVersion, currentReleaseVersion)
+	}
+
 	learn.API = api
 }
