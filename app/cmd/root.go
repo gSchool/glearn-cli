638c660d8206e7c3ef0e2d5ca764821081e573b5
Peter Grunde <peter.a.grunde@gmail.com>
2023-11-15 15:10:40 -0700

    bump version tag

--- app/cmd/root.go
+++ app/cmd/root.go
@@ -20,7 +20,7 @@ Please set your API token with this command: learn set --api_token=<your_api_tok
 You can get your api token at https://learn-2.galvanize.com/api_token`
 
 // currentReleaseVersion is used to print the version the user currently has downloaded
-const currentReleaseVersion = "v0.10.11"
+const currentReleaseVersion = "v0.10.12"
 
 // rootCmd is the base for all our commands. It currently just checks for all the
 // necessary credentials and prompts the user to set them if they are not there.
